e39ada5a7c13f0b493545f8898d06d14a57d4b82
Marina Lex <marina.lex@bccn-berlin.de>
2024-07-04 11:49:21 +0200

    Add GitHub Actions configuration

--- inflammation_analysis.py
+++ inflammation_analysis.py
@@ -21,16 +21,9 @@ def main(arguments):
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-<<<<<<< HEAD:inflammation-analysis.py
         view_data = {'average': models.daily_mean(inflammation_data), 
                      'max': models.daily_max(inflammation_data), 
                      'min': models.daily_min(inflammation_data)}
-
-=======
-        view_data = {'average': models.daily_mean(inflammation_data),
-                     'max': models.daily_max(inflammation_data),
-                     'min': models.daily_min(inflammation_data)}
->>>>>>> style-fixes:inflammation_analysis.py
         views.visualize(view_data)
 
 if __name__ == "__main__":
